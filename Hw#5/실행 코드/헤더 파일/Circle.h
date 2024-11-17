196fb98e677ad69bab46e04ad2f37a2e6f3da5ec
JHubO420 <ijkl64818@gmail.com>
2024-11-17 15:09:16 +0900

    Add files via upload

---
+++ Hw#5/실행 코드/헤더 파일/Circle.h
@@ -0,0 +1,15 @@
+#ifndef CIRCLE_H
+#define CIRCLE_H
+
+#include <iostream>
+#include "Shape.h"  
+using namespace std;
+
+class Circle : public Shape {
+protected:
+    virtual void draw();  
+};
+
+#endif // CIRCLE_H
+
+
