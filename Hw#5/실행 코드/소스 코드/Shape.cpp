b9270908fad74cfaba70be639427c0a63476e7e5
JHubO420 <ijkl64818@gmail.com>
2024-11-17 15:10:31 +0900

    Add files via upload

---
+++ Hw#5/실행 코드/소스 코드/Shape.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Shape.h"
+using namespace std;
+
+void Shape::paint() {
+    draw();  
+}
+
+
+//void Shape::draw() {
+//    cout << "--Shape--" << endl;  
+//}
+
+//Shape::~Shape() {
+//    
+//}
+
+Shape* Shape::add(Shape* p) {
+    this->next = p;
+    return p;
+}
